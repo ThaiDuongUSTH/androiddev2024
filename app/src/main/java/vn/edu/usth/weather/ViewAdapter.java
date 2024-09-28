c401c1094149af0ec9f0241e2b9dcec26504bdf4
ThaiDuongUSTH <duongpt.ba12-057@st.usth.edu.vn>
2024-09-28 15:27:42 +0700

    prac11+12

--- app/src/main/java/vn/edu/usth/weather/ViewAdapter.java
+++ app/src/main/java/vn/edu/usth/weather/ViewAdapter.java
@@ -2,12 +2,15 @@ package vn.edu.usth.weather;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 public class ViewAdapter extends FragmentStateAdapter {
+    private static final int NUM_Page = 3;
 
-    public VIewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+    public ViewAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
         super(fragmentManager, lifecycle);
     }
 
@@ -16,24 +19,22 @@ public class ViewAdapter extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position) {
             case 0:
-                return new Tab1Fragment();
+                return new allPage();
             case 1:
-                return new Tab2Fragment();
+                return new allPage();
             case 2:
-                return new Tab3Fragment();
-            case 3:
-                return new Tab4Fragment();
+                return new allPage();
 
             default:
-                return new Tab1Fragment();
+                return new allPage();
         }
 
     }
 
     @Override
     public int getItemCount() {
-        return 4;
+        return NUM_Page;
     }
 }
-{
-}
+
+
